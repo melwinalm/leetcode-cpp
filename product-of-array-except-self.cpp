36f3046811fcf94add8a02ca6deb69d1b63078a2
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-06-19 21:07:41 +0530

    Update product-of-array-except-self.cpp

--- product-of-array-except-self.cpp
+++ product-of-array-except-self.cpp
@@ -33,3 +33,25 @@ public:
         return output;
     }
 };
+
+// Using Prefix and Suffix array by using output array itself instead of extra space
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> output(n, 1);
+
+        for(int i = 1; i < n; i++){
+            output[i] = output[i-1] * nums[i-1];
+        }
+
+        int curr = 1;
+        for(int i = n-1; i >= 0; i--){
+            output[i] = output[i] * curr;
+            curr *= nums[i];
+        }
+
+        return output;
+    }
+};
