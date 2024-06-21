6b97cad23a4a49dd247574e015f5a5ed587ec468
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-06-21 20:35:57 +0530

    Update valid-palindrome.cpp

--- valid-palindrome.cpp
+++ valid-palindrome.cpp
@@ -26,4 +26,30 @@ public:
     }
 };
 
-// 
+// Two Pointer Approach
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int N = s.size();
+        int i = 0;
+        int j = N - 1;
+
+        while(i < j){
+            if(!isalnum(s[i])){
+                i++;
+            }
+            else if(!isalnum(s[j])){
+                j--;
+            }
+            else if(tolower(s[i]) != tolower(s[j])){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+    }
+};
