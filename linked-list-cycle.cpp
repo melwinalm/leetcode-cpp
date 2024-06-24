5851dcfc75627e46acb05f5a331377f35c8e1e60
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-06-24 17:49:36 +0530

    Create linked-list-cycle.cpp

---
+++ linked-list-cycle.cpp
@@ -0,0 +1,28 @@
+/*
+Title: Linked List Cycle
+Link: https://leetcode.com/problems/linked-list-cycle/
+*/
+
+// Tortoise and Hare Algorithm
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL){
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
