c6c4dd26bd4151bfe7dfed495031ff8c77cc5f37
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-06-25 17:53:09 +0530

    Update maximum-depth-of-binary-tree.cpp

--- maximum-depth-of-binary-tree.cpp
+++ maximum-depth-of-binary-tree.cpp
@@ -14,3 +14,36 @@ public:
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
+
+// Breadth First Search
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        
+        queue<TreeNode*> bfsQueue;
+        bfsQueue.push(root);
+
+        int depth = 0;
+        while(!bfsQueue.empty()){
+            int count = bfsQueue.size();
+            depth++;
+            while(count > 0){
+                TreeNode* temp = bfsQueue.front();
+                bfsQueue.pop();
+
+                if(temp->left){
+                    bfsQueue.push(temp->left);
+                }
+                if(temp->right){
+                    bfsQueue.push(temp->right);
+                }
+                count--;
+            }
+        }
+
+        return depth;
+    }
+};
