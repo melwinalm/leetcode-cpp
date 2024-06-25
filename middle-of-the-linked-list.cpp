9fbfcc9ab00187eb19be705f38a05606e615da2b
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-06-25 15:21:26 +0530

    Update middle-of-the-linked-list.cpp

--- middle-of-the-linked-list.cpp
+++ middle-of-the-linked-list.cpp
@@ -29,3 +29,19 @@ public:
         return head;
     }
 };
+
+// Slow and Fast pointer approach
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
