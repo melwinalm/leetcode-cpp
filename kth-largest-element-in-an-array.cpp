1b8e4c996a49b6e7cf42797880f33d259d90a152
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-06-27 18:47:07 +0530

    Update kth-largest-element-in-an-array.cpp

--- kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array.cpp
@@ -14,3 +14,25 @@ public:
         return nums[k-1];
     }
 };
+
+// Using Priority Queue
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+
+        for(int num: nums){
+            pq.push(num);
+        }
+
+        int output;
+
+        while(k > 0){
+            output = pq.top();
+            pq.pop();
+            k--;
+        }
+
+        return output;
+    }
+};
