393d2766789585c5396549af7517b56c01a15d7f
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-07-03 19:34:35 +0530

    Update network-delay-time.cpp

--- network-delay-time.cpp
+++ network-delay-time.cpp
@@ -63,3 +63,60 @@ public:
         return minTime;
     }
 };
+
+// Using Bellman Ford Algorithm
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int, vector<pair<int,int>>> adj;
+
+        // Construct the adjaceny list
+        for(auto time: times){
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+
+            adj[u].push_back({v, w});
+        }
+
+        // Create a wieghts array witn default value as infinity and source value as 0
+        vector<int> weights(n, INT_MAX);
+        weights[k-1] = 0;
+
+        // Push the source node t oqueue
+        queue<int> q;
+        q.push(k);
+
+        while(!q.empty()){
+            // Pop the node from the queue
+            int currnode = q.front();
+            q.pop();
+            int currweight = weights[currnode-1];
+
+            // Check all the neighbors of the popped node
+            for(pair<int, int> neighbor: adj[currnode]){
+                int neighnode = neighbor.first;
+                int neighweight = neighbor.second;
+
+                // If the neighbor node distance is greater than the popped node ditance + edge value from popped node to neighbor node then update the weight and also push it into the queue again for updating the subsequent set of values again
+                if(currweight + neighweight < weights[neighnode-1]){
+                    weights[neighnode-1] = currweight + neighweight;
+                    q.push(neighnode);
+                }
+            }
+        }
+
+        int minTime = 0;
+
+        for(int w: weights){
+            if(w == INT_MAX){
+                return -1;
+            }
+            else{
+                minTime = max(minTime, w);
+            }
+        }
+
+        return minTime;
+    }
+};
