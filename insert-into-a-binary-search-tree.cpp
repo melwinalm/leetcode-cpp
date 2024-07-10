40158afb24977f1cfb974d8ea1e7a399f105d21d
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-07-10 17:54:04 +0530

    Update insert-into-a-binary-search-tree.cpp

--- insert-into-a-binary-search-tree.cpp
+++ insert-into-a-binary-search-tree.cpp
@@ -35,3 +35,21 @@ public:
         return root;
     }
 };
+
+// Recursive Approach
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == NULL){
+            return new TreeNode(val);
+        }
+        else if(val < root->val){
+            root->left = insertIntoBST(root->left, val);
+        }
+        else{
+            root->right = insertIntoBST(root->right, val);
+        }
+
+        return root;
+    }
+};
