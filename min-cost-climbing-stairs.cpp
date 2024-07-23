79fa37db27dcac75864e93f2af9970d04aa31961
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-07-23 18:21:35 +0530

    Create min-cost-climbing-stairs.cpp

---
+++ min-cost-climbing-stairs.cpp
@@ -0,0 +1,27 @@
+/*
+Title: Min Cost Climbing Stairs
+Link: https://leetcode.com/problems/min-cost-climbing-stairs
+*/
+
+// DP + Memoization
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+
+        if(n == 2){
+            return min(cost[0], cost[1]);
+        }
+
+        vector<int> dp(n);
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < n; i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+
+        return min(dp[n-1], dp[n-2]);
+    }
+};
