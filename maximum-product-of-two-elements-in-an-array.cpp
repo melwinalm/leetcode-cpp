d3bd6474c52ce8eff523a87efaab2111aea7b465
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-07-25 18:17:58 +0530

    Update maximum-product-of-two-elements-in-an-array.cpp

--- maximum-product-of-two-elements-in-an-array.cpp
+++ maximum-product-of-two-elements-in-an-array.cpp
@@ -20,3 +20,13 @@ public:
         return (num1 - 1) * (num2 - 1);
     }
 };
+
+// Using Partial Sort
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        partial_sort(nums.begin(), nums.begin()+2, nums.end(), greater<int>());
+
+        return (nums[0] - 1) * (nums[1] - 1);
+    }
+};
