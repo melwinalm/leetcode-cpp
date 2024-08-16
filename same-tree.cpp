76b9baccad9ecce695774b28da75ecddebe00ee0
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-08-16 19:40:12 +0530

    Create same-tree.cpp

---
+++ same-tree.cpp
@@ -0,0 +1,18 @@
+/*
+Title: Same Tree
+Link: https://leetcode.com/problems/same-tree/
+*/
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL){
+            return true;
+        }
+        else if(p == NULL || q == NULL){
+            return false;
+        }
+
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
