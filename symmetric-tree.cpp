67086889c1ef6a08ffce0ae94d18f98c02de830d
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-08-27 17:39:13 +0530

    Update symmetric-tree.cpp

--- symmetric-tree.cpp
+++ symmetric-tree.cpp
@@ -3,26 +3,22 @@ Title: Symmetric Tree
 Link: https://leetcode.com/problems/symmetric-tree/
 */
 
-// Using Depth First Search
+// Recursive 
 class Solution {
-public:
-    bool isSymmetric(TreeNode* root) {
-        if(!root){
+private:
+    bool isSymmetric(TreeNode* left, TreeNode* right){
+        if(left == NULL && right == NULL){
             return true;
         }
-
-        return isSymmetric(root->left, root->right);
-    }
-
-    bool isSymmetric(TreeNode* left, TreeNode* right){
-        if(!left || !right){
-            return left == right;
-        }
-
-        if(left->val != right->val){
+        else if(left == NULL || right == NULL){
             return false;
         }
 
-        return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
+        return left->val == right->val && isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
+    }
+
+public:
+    bool isSymmetric(TreeNode* root) {
+        return isSymmetric(root->left, root->right);
     }
 };
