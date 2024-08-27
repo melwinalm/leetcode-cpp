027a148bbba6ecb3d9c6d184bef29312fc998778
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-08-27 17:31:11 +0530

    Create merge-two-sorted-lists.cpp

---
+++ merge-two-sorted-lists.cpp
@@ -0,0 +1,26 @@
+/*
+Title: Merge Two Sorted Lists  https://leetcode.com/problems/merge-two-sorted-lists/
+Link: https://leetcode.com/problems/merge-two-sorted-lists
+*/
+
+// Recursive
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL){
+            return list2;
+        }
+        else if(list2 == NULL){
+            return list1;
+        }
+
+        if(list1->val < list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
