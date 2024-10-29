50919de7b58bfadad29ee9adf48bc37cc70368b9
Melwin Amith D'Almeida <melwinalm@gmail.com>
2024-10-29 16:37:18 +0530

    Update car-pooling.cpp

--- car-pooling.cpp
+++ car-pooling.cpp
@@ -25,3 +25,29 @@ public:
         return true;
     }
 };
+
+// Sweep Line Algorithm
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int> cap(1001);
+        
+        for(vector<int> trip: trips){
+            int numPassengers = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+
+            cap[from] += numPassengers;
+            cap[to] -= numPassengers;
+        }
+
+        for(int item: cap){
+            capacity -= item;
+            if(capacity < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
