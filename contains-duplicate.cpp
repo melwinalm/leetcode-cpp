e6a5b83c9610883dc70360ba7fbe85892463afab
Melwin Amith D'Almeida <melwinalm@gmail.com>
2025-05-20 21:51:24 +0530

    Update contains-duplicate.cpp

--- contains-duplicate.cpp
+++ contains-duplicate.cpp
@@ -2,6 +2,27 @@
 Title: Contains Duplicate
 Link: https://leetcode.com/problems/contains-duplicate/
 */
+
+// Using Set
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> duplicate;
+
+        for(int val : nums){
+            if(duplicate.find(val) != duplicate.end()){
+                return true;
+            }
+            else{
+                duplicate.insert(val);
+            }
+        }
+
+        return false;
+    }
+};
+
+// Using Sorting
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
