9f599556b889d9114b2a3b87836d40e4ee0909a0
Melwin Amith D'Almeida <melwinalm@gmail.com>
2025-05-27 21:15:46 +0530

    Update merge-two-sorted-lists.cpp

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -24,3 +24,51 @@ public:
         }
     }
 };
+
+// Iterative
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL){
+            return list2;
+        }
+        if(list2 == NULL){
+            return list1;
+        }
+
+        ListNode* head;
+
+        if(list1->val < list2->val){
+            head = list1;
+            list1 = list1->next;
+        }
+        else{
+            head = list2;
+            list2 = list2->next;
+        }
+
+        ListNode* temp = head;
+
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val < list2->val){
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                temp->next = list2;
+                list2 = list2->next;
+            }
+
+            temp = temp->next;
+        }
+
+        if(list1 == NULL){
+            temp->next = list2;
+        }
+        else{
+            temp->next = list1;
+        }
+
+        return head;
+    }
+};
